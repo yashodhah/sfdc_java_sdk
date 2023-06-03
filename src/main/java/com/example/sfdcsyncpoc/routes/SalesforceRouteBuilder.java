4d1769d3dd289712843cc8873cbc21ca0fa76e02
yashodhah <y.dominatz@gmail.com>
2023-06-03 17:17:51 +0530

    add topic based enhancement

--- src/main/java/com/example/sfdcsyncpoc/routes/SalesforceRouteBuilder.java
+++ src/main/java/com/example/sfdcsyncpoc/routes/SalesforceRouteBuilder.java
@@ -7,8 +7,13 @@ import org.springframework.stereotype.Component;
 public class SalesforceRouteBuilder extends RouteBuilder {
     @Override
     public void configure() {
-        from("salesforce:subscribe:{{salesforce.topic}}")
-                .unmarshal().json()
-                .log(">>> ${body}");
+//        from("salesforce:subscribe:{{salesforce.topic}}")
+//                .unmarshal().json()
+//                .log(">>> ${body}");
+
+        from("direct:processData")
+                .log("Salesforce camel started")
+                .to("salesforce:query?sObjectQuery=SELECT Name FROM Account")
+                .log("Received Salesforce response: ${body}");
     }
 }
